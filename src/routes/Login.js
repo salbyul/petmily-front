302e5b4987a79454b1e398aecf77d39babc15b0f
salbyul <kpast96@gmail.com>
2022-10-18 18:11:00 +0900

    add routes / Login

---
+++ src/routes/Login.js
@@ -0,0 +1,11 @@
+function Login() {
+    return (
+        <>
+            <div className="flex items-center place-content-evenly">
+                <div className="h-96 bg-red-400">This is Image Section.</div>
+                <div className="bg-blue-300">This is Form Section.</div>
+            </div>
+        </>
+    );
+}
+export default Login;
